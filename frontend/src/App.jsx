13df5110943854a16bd0f0bf14dcad70b544e47d
saimanideep <saimanideep765@gmail.com>
2025-03-11 14:06:21 +0530

    adding files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
+import {useState} from 'react'
 import './App.css'
 import Login from './components/Login'
+import Signup from './components/Signup'
 
 function App() {
   
   return (
     <>
       <Login/>
+      <signup/>
     </>
   )
 }
