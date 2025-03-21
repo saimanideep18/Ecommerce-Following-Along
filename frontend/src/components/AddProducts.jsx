0998db631f915a55b46d4efe346c9db7c4748274
saimanideep <saimanideep765@gmail.com>
2025-03-21 15:14:23 +0530

    1

--- frontend/src/components/AddProducts.jsx
+++ frontend/src/components/AddProducts.jsx
@@ -1,32 +1,72 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
-const AddProducts = () => {
-    const [noOfImages,setNoOfImages] = useState(1);
+const AddProduct = () => {
+    const [noOfImages,setNoOfImages] = useState(new Array(1));
+    const [productDetails,setProductDetails]=useState({
+        name:"",
+        email:"",
+        password:"",        
+    })
+    const [productImages,setProductImages]=useState([])
+
+    async function handelSubmit(ele){
+        try{
+            const {name,email,password} = productDetails;
+            if(!name||!email||!password || productImages.length ==0){
+                alert("add all fields")
+                return;
+            }
+            const formData = new FormData();
+            await axios.post("http://localhost:5173/product/AddProducts")
+             
+         }catch(err){
+            console.log(err);
+            alert("something went  wrong while sending data")
+        }
+
+    }
+
   return (
     <div>
-      <form action="">
-        <input type="text" name={"title"} placeholder='Enter title....'/>
-        <input type="text" name={"description"} placeholder='Enter product description...'/>
-        <select name="" id="" onChange={(event)=>{
-            console.log(event.target)
-            setNoOfImages(new Array(parseInt(event.target.value)).fill(1));
-        }}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-        </select>
-        <label htmlFor="">Add Images</label>
-        {
-            noOfImages.map((ele)=>(
-                <input type= "file" accept='image/*'/>
-            ))
-        }
-      </form>
+        <form action="">
+            <input type="text" name={"title"} placeholder='Enter Title....' onChange={(event)=>{
+                setProductDetails({...productDetails,[event.target.name]:event.target.value})
+            }} />
+            <input type="text" name={"description"} placeholder='Enter Product description....' onChange={(event)=>{
+                setProductDetails({...productDetails,[event.target.name]:event.target.value})
+            }} />
+            <input type="number" name="price" placeholder='Enter price...' onChange={(event)=>{
+                    setProductDetails({...productDetails,[event.target.name]:event.target.value})
+                }}
+             />
+            <select name="" id="" onChange={(event)=>{
+                console.log(event.target.value,noOfImages)
+                setNoOfImages(new Array(parseInt(event.target.value)).fill(1))
+
+
+            }}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+            </select>
+            <label htmlFor="">Add Images</label>
+            {
+                noOfImages.map((ele)=>{
+                    <input type='file' accept='image/*' onChange={(event)=>{
+                        setProductImages([...productImages,event.target.files[0]])
+                    }}></input>
+                    
+                })
+            }
+        </form>
+      
     </div>
   )
 }
 
-export default AddProducts
+export default AddProduct
